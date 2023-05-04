120ebe599de0c39329acf459a3da31df053cca8a
lombazzic <samuele.lombardi@studenti.ittsrimini.edu.it>
2023-05-04 09:09:19 +0200

    versione prima

---
+++ lombardi.samuele.4h.firstdb/Program.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+
+
+
+Console.WriteLine("Hello, DBWorld!");
+
+SQLiteConnection cn1 = new SQLiteConnection("chinook.db");
+var tblArtist = cn1.Query<Artist>("select * from artists");
+
+Console.WriteLine($"In questa tabella ci sono {tblArtist.Count} record" );
+
+
+
+// Fine del main --- inizio delle dichiarazioni ---
+public class Artist
+{
+    public int ArtistId{ get; set; }
+    public string Name{ get; set; }
+}
